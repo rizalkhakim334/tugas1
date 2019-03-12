31cea844b1939279368c75884424334a29d8e09b
rizalkhakim334 <48473541+rizalkhakim334@users.noreply.github.com>
2019-03-12 16:08:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>Hello World</h1> <br/><table>
+    <tr>
+        <td>NIM</td>
+        <td> : </td>
+        <td>16.01.53.0151</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td> : </td>
+        <td>RIZAL KHAKIM</td>
+    </tr>
+</table>
+<a href="/unisbank/unisbank">Go To Akademik</a>
